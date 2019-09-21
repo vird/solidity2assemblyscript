e2bac5f8540a8abcfccd9617003f8557753a9912
kstasi <nastasiakondaurova@gmail.com>
2019-09-21 17:11:53 +0300

    two contracts added

---
+++ assemblyscript_contracts/forer.ts
@@ -0,0 +1,25 @@
+import { storage, logging } from "near-runtime-ts";
+
+let value:i32;
+export function Forer__forer():i32 {
+  let a:i32 = 1;
+  let b:i32 = 1;
+  let c:i32 = 1;
+  let bb:boolean;
+  c = -(c);
+  c = (a + b);
+  c = (a - b);
+  c = (a * b);
+  c = (a / b);
+  c = (a % b);
+  c = (a & b);
+  c = (a | b);
+  c = (a ^ b);
+  bb = (a == b);
+  bb = (a != b);
+  bb = (a < b);
+  bb = (a <= b);
+  bb = (a > b);
+  bb = (a >= b);
+  return c;
+};
