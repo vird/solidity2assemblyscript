3cd17a5bbf824d33d2d9dc8578bee9af56b7aa81
kstasi <nastasiakondaurova@gmail.com>
2019-09-21 21:21:57 +0300

    update

--- solidity_contracts/Arrays.sol
+++ solidity_contracts/Arrays.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.5.11;
 
-contract Mapper {
-  mapping (address => uint) private balances; 
-  function ifer() public payable {
-    require((balances[msg.sender] + msg.value) >= balances[msg.sender]);
-    balances[msg.sender] += msg.value;
-  }
+contract Array { 
+    function f(uint len) public {
+        uint[] memory a = new uint[](7);
+        bytes memory b = new bytes(len);
+        a[6] = 8;
+    }
 }
